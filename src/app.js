26dd2dbad3ff53f81055c958a30f42e06af6a238
LarysaBilychenko <116347412+LarysaBilychenko@users.noreply.github.com>
2022-11-11 12:44:32 +0100

    added current time

--- src/app.js
+++ src/app.js
@@ -1,5 +1,28 @@
 let apiKey = "3af0ace7e53bde08dtbd8a6b4o60a6d7";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Kyiv&key=${apiKey}&units=metric`;
+function formatData(timestamp) {
+  let date = new Date(timestamp);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day}, ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.city;
@@ -9,6 +32,8 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.temperature.humidity;
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = response.data.wind.speed;
+  let currentTimeElement = document.querySelector("#current-time");
+  currentTimeElement.innerHTML = formatData(response.data.time * 1000);
   let temperatureElement = document.querySelector(".temperature");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
 }
