245e4ebc35f26e8bfd78b7625317eab816c9ebfe
LarysaBilychenko <116347412+LarysaBilychenko@users.noreply.github.com>
2022-11-15 16:39:31 +0100

    added fahrenheit to celsius conversion

--- src/app.js
+++ src/app.js
@@ -61,12 +61,23 @@ let celsiusTemperature = null;
 
 function displayFahrenheit(event) {
   event.preventDefault();
+  //celsiusLink.classList.remove("active");
+  //fahrenheitLink.classList.add("active");
   let temperatureElement = document.querySelector(".temperature");
   let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
 
+function displayCelsius(event) {
+  event.preventDefault();
+  //celsiusLink.classList.add("active");
+  //fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector(".temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+search("Kyiv");
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheit);
 
-search("Kyiv");
+let celsiusLink = document.querySelector("celsius-link");
+celsiusLink.addEventListener("click", displayCelsius);
